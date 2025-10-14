b8136d08785ff6982296d5c6b8743331f25e9311
Thiago Tasseli <thiagotasseli@outlook.com.br>
2025-10-13 21:03:33 -0300

    subindo exercicio de logica

---
+++ src/Exercicio02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o raio da esfera: ");
+        double raio = scan.nextDouble();
+
+        double pi = 3.1415;
+
+        double volume = (4.0 / 3.0) * pi * (raio * raio * raio);
+        double area = 4 * pi * (raio * raio);
+
+        System.out.printf("\nVolume da esfera: %.2f\n", volume);
+        System.out.printf("Área da superfície: %.2f\n", area);
+    }
+}
---
+++ src/Exercicio03.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite as suas quatro notas: ");
+        double n1 = scan.nextDouble();
+        double n2 = scan.nextDouble();
+        double n3 = scan.nextDouble();
+        double n4 = scan.nextDouble();
+
+        double media = (n1 + n2 + n3+ n4) / 4;
+
+        System.out.printf("A média das notas é %.2f", media);
+    }
+}
